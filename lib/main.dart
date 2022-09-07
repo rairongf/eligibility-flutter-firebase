3bad9006b7c6d81b3056f26ffe7d82fbc6e90365
Rairon Ferreira <rairon.ferreira@gec.inatel.br>
2022-09-07 20:36:42 -0300

    chore: setup dependency injection

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,19 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:dio/dio.dart';
+import 'package:eligibility_flutter_firebase/repositories/eligibilityForms.repository.dart';
 import 'package:flutter/material.dart';
-
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 import 'app.dart';
+import 'pages/controller.dart';
 
-void main() {
-  runApp(const EligibilityFormsList());
+void main() async {
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  final httpClient = Dio();
+  final storageClient = FirebaseFirestore.instance;
+  final repository = EligibilityFormsRepository(httpClient, storageClient);
+  final pageController = EligibilityFormsListController(repository);
+  runApp(EligibilityFormsListPage(pageController));
 }

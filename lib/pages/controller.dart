69d0c5b811d7f708c0dc5bb9c7ab1a87b98d62ec
Rairon Ferreira <rairon.ferreira@gec.inatel.br>
2022-09-07 20:35:12 -0300

    feat: add page controller

---
+++ lib/pages/controller.dart
@@ -0,0 +1,16 @@
+import 'package:eligibility_flutter_firebase/entities/eligibilityForm.entity.dart';
+
+import '../repositories/eligibilityForms.repository.dart';
+
+class EligibilityFormsListController {
+  final EligibilityFormsRepository repository;
+  late final List<EligibilityForm> formsList;
+
+  EligibilityFormsListController(this.repository);
+
+  void initialize() async {
+    final formsJsonList = await repository.getFormsJsonList();
+    await repository.storeDocsOnCollection(formsJsonList, 'forms');
+    formsList = await repository.queryDocsFromCollection('forms');
+  }
+}

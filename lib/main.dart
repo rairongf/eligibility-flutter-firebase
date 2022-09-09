70c6511270914716939bcd2a6ae6b2ed1a54fd0a
Rairon Ferreira <rairon.ferreira@gec.inatel.br>
2022-09-08 22:44:01 -0300

    fix: ensure widgets binding initialized

--- lib/main.dart
+++ lib/main.dart
@@ -5,9 +5,10 @@ import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
 import 'app.dart';
-import 'pages/controller.dart';
+import 'controllers/formsList.controller.dart';
 
 void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );

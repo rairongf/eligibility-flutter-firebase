ba15a2aadc7638c32718eb3e3a245f97b86365b0
Rairon Ferreira <rairon.ferreira@gec.inatel.br>
2022-09-08 22:42:34 -0300

    refactor: add list view to forms list page

--- lib/pages/formsList.page.dart
+++ lib/pages/formsList.page.dart
@@ -1,7 +1,14 @@
+import 'package:eligibility_flutter_firebase/widgets/form.item.dart';
 import 'package:flutter/material.dart';
+import 'package:shimmer/shimmer.dart';
+
+import '../controllers/formsList.controller.dart';
+import '../entities/eligibilityForm.entity.dart';
 
 class FormsListPage extends StatefulWidget {
-  const FormsListPage({Key? key, required this.title}) : super(key: key);
+  final EligibilityFormsListController controller;
+  const FormsListPage(this.controller, {Key? key, required this.title})
+      : super(key: key);
 
   final String title;
 
@@ -10,6 +17,27 @@ class FormsListPage extends StatefulWidget {
 }
 
 class _FormsListPageState extends State<FormsListPage> {
+  EligibilityFormsListController get controller => widget.controller;
+  List<EligibilityForm> filteredList = [];
+
+  @override
+  void initState() {
+    controller.initialize().then((_) {
+      setState(() {
+        filteredList = controller.formsList;
+      });
+    });
+    super.initState();
+  }
+
+  void _filterListByStatus(String status) {
+    final updatedList =
+        filteredList.where((form) => form.displayStatus == status).toList();
+    setState(() {
+      filteredList = updatedList;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -17,11 +45,46 @@ class _FormsListPageState extends State<FormsListPage> {
         title: Text(widget.title),
       ),
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: const [],
-        ),
+        child: ListView.separated(
+            itemBuilder: (_, index) {
+              if (filteredList.isEmpty) {
+                return _buildLoadingListTile(context);
+              }
+              final form = filteredList[index];
+              return EligibilityFormListItem(form);
+            },
+            separatorBuilder: (_, __) => const SizedBox(height: 10),
+            itemCount: filteredList.isEmpty ? 3 : filteredList.length),
       ),
     );
   }
+}
+
+Widget _buildLoadingListTile(BuildContext context) {
+  return ListTile(
+    title: Shimmer.fromColors(
+      highlightColor: Colors.grey.shade300,
+      baseColor: const Color.fromRGBO(242, 242, 242, 1),
+      child: Container(
+        margin: EdgeInsets.only(right: MediaQuery.of(context).size.width * 0.6),
+        height: 15,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(12),
+          color: const Color.fromRGBO(242, 242, 242, 1),
+        ),
+      ),
+    ),
+    subtitle: Shimmer.fromColors(
+      highlightColor: Colors.grey.shade300,
+      baseColor: const Color.fromRGBO(242, 242, 242, 1),
+      child: Container(
+        margin: EdgeInsets.only(right: MediaQuery.of(context).size.width * 0.3),
+        height: 12,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(12),
+          color: const Color.fromRGBO(242, 242, 242, 1),
+        ),
+      ),
+    ),
+  );
 }

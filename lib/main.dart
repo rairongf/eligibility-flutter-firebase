ebea29eb9656b66dab8ac5f93cf1786cc12719d9
Rairon Ferreira <rairon.ferreira@gec.inatel.br>
2022-09-09 23:16:12 -0300

    chore: initialize date formatting

--- lib/main.dart
+++ lib/main.dart
@@ -3,12 +3,14 @@ import 'package:dio/dio.dart';
 import 'package:eligibility_flutter_firebase/repositories/eligibilityForms.repository.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:intl/date_symbol_data_local.dart';
 import 'firebase_options.dart';
 import 'app.dart';
 import 'controllers/formsList.controller.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  initializeDateFormatting('pt_BR');
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
